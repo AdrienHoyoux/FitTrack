3aebde95e2cf26efa80a0b68ecbae3087a50f891
Hoyoux Adrien <adrien.hoyoux@student.hepl.be>
2024-06-24 21:35:10 +0200

    home page design

---
+++ lib/screens/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+class Home extends StatefulWidget {
+  static String routeName = '/home';
+  Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
